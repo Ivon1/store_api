095743ff6d8af9c924538dcff19c1eb25b264b9f
Ivan Marchenko <vania9023@gmail.com>
2026-04-06 18:55:09 +0100

    Add get all products by category id and by product id

--- src/main/java/com/codewithmosh/store/controllers/UserController.java
+++ src/main/java/com/codewithmosh/store/controllers/UserController.java
@@ -5,12 +5,12 @@ import com.codewithmosh.store.entities.User;
 import com.codewithmosh.store.mappers.UserMapper;
 import com.codewithmosh.store.repositories.UserRepository;
 import lombok.AllArgsConstructor;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Set;
 
 @RestController
 @AllArgsConstructor
@@ -20,8 +20,13 @@ public class UserController {
     private final UserMapper userMapper;
 
     @GetMapping
-    public Iterable<UserDto> getUsers() {
-        return userRepository.findAll()
+    public Iterable<UserDto> getUsers(
+            @RequestParam(required = false, defaultValue = "") String sort
+    ) {
+        if(!Set.of("name","email").contains(sort))
+            sort = "name";
+
+        return userRepository.findAll(Sort.by(sort))
                 .stream()
                 .map(user -> userMapper.toDto(user))
                 .toList();
